f21dd994519246231fea1ac1ea882be342c479c3
Megan Moroney <memoroney15@att.net>
2020-06-20 17:45:03 -0400

    adding final weatherpy and vacationpy files, images, csv and analysis

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "39ff9e3aa4844fb34b2aba020fb46ced"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCbILTKtwZPeTAss1Q1pVukMuPSydCl6SA"
